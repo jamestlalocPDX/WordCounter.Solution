99434cd900ac22107e99c32042729fcb1ce09a1a
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-06 16:18:29 -0800

    set the directives, namespace, and test structure.

---
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.TestTools
+{
+  [TestClass]
+  public class RepeatCounterTests
+  {
+    [TestMethod]
+    
+  }
+}
