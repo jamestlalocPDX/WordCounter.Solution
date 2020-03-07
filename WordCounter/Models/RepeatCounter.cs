21330d8e202f3a3e43ad0e100a7085c10003da02
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-07 13:02:28 -0800

    add 'sentence' auto-implemented property, and overloaded constructor.

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -6,10 +6,16 @@ namespace WordCounter.Models
   public class RepeatCounter
   {
     public string Word {get; set; }
+    public string Sentence { get; set; }
   
     public RepeatCounter(string word)
     {
       Word = word;
+    }
+    public RepeatCounter (string word, string sentence)
+    : this(word)
+    {
+      Sentence = sentence;
     }
     public string wordCheck()
     {
