915dd882b2d1139ff39c44c341bcb255c3065fbd
Jonathan Carlos <jonathancarlos21@hotmail.com>
2020-03-06 17:07:40 -0800

    add auto-implemented fields and a constructor.

--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -5,6 +5,13 @@ namespace WordCounter.Models
 {
   public class RepeatCounter
   {
+    public string Word {get; set; }
+    public string Sentence { get; set; }
+    public RepeatCounter(string word, string sentence)
+    {
+      Word = word;
+      Sentence = sentence;
+    }
     public string wordCheck()
     {
       reutrn "word";
